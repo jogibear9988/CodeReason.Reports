5cef1bc9c8c17daa840288d6d205960091bcdf57
jkuehner <jochen.kuehner@kardex.com>
2012-12-03 23:13:12 +0100

    Include Zen Framework (http://barcoderender.codeplex.com/) for other Barcode support...

---
+++ CodeReason.Reports/Document/Barcode/TableCellBarcodeZenFramework.cs
@@ -0,0 +1,23 @@
+﻿/************************************************************************
+ * Copyright: Hans Wolff
+ *
+ * License:  This software abides by the LGPL license terms. For further
+ *           licensing information please see the top level LICENSE.txt 
+ *           file found in the root directory of CodeReason Reports.
+ *
+ * Author:   Hans Wolff
+ *
+ ************************************************************************/
+
+using CodeReason.Reports.Barcode;
+using CodeReason.Reports.Interfaces;
+
+namespace CodeReason.Reports.Document.Barcode
+{
+    /// <summary>
+    /// Table cell barcode ZenFramework
+    /// </summary>
+    public class TableCellBarcodeZenFramework : BarcodeZenFramework, ITableCellValue
+    {
+    }
+}
